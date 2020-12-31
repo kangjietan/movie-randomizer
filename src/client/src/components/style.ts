f000cf444372e8cbbd42e8ec2d4526032f6dd4d8
kangjietan <kangjietan@gmail.com>
2020-12-30 23:48:27 -0800

    App styles

---
+++ src/client/src/components/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const RandomMovieContainer = styled.div``;
+
+export const RandomMovieTitle = styled.div``;
